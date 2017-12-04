f67ebfd3c9df723d77aea73d36e42d62e9cbf574
ShinobuFormula <damien.laporte@viacesi.fr>
2017-12-04 13:15:56 +0100

    dsd

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ExempleMsg() printf("Coucou \n"); // Macro sans paramètre//
+
+#define ExempleAge(age) if (age >= 18) \
+                                            printf("Vous etes majeur \n");  //Macro avec paramètres//
+
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
+ExempleMsg()           //Appelle a macro sans paramètre//
+
+
+ExempleAge(18)          //Apelle a macro avec paramètre//
+
+
+return 0;
 }
